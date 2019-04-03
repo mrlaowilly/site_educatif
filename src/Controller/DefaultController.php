f826c9b838c473b970678aa818f60f55140779e6
Oliver <olivier.himmer@gmail.com>
2019-04-03 14:31:36 +0200

    changement default/home

--- src/Controller/DefaultController.php
+++ src/Controller/DefaultController.php
@@ -2,19 +2,56 @@
 
 namespace App\Controller;
 
+use App\Entity\User;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Doctrine\ORM\EntityManagerInterface;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class DefaultController extends AbstractController
 {
     /**
+     * @return Response
      * @Route("/home", name="home")
      */
     public function index()
     {
-        return $this->render('default/index.html.twig', [
-            'controller_name' => 'DefaultController',
+        return $this->render('default/index.html.twig');
+    }
 
+    /**
+     * @Route("/register", name="register")
+     */
+    public function register(
+        Request $request,
+        EntityManagerInterface $manager,
+        UserPasswordEncoderInterface $encoder
+        ) {
+        $user = new User();
+        $form = $this->createFormBuilder($user)
+            ->add('email', EmailType::class)
+            ->add('password', PasswordType::class)
+            ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $user->setRoles(['ROLE_USER']);
+
+            $encoded = $encoder->encodePassword($user, $form['password']->getData());
+            $user->setPassword($encoded);
+
+            $manager->persist($user);
+            $manager->flush();
+            return $this->redirectToRoute('home');
+        }
+
+        return $this->render('default/register.html.twig', [
+            'register_form'=> $form->createView()
         ]);
     }
 }
