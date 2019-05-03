0868886a1b70fe4453098a7b336c499d19447ada
anis idrenmouche <anisidrenmouche@hotmail.fr>
2019-05-03 17:37:55 +0200

    j'ai rajouter des fixtures, 03/05/2019

--- src/DataFixtures/PagesFixtures.php
+++ src/DataFixtures/PagesFixtures.php
@@ -29,47 +29,48 @@ class PagesFixtures extends Fixture implements DependentFixtureInterface
             ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG2));
         $manager->persist($pages2);
 
-        $pages2 = new Page();
-        $pages2->setTitle( 'Les ddd')
-            ->setContent('Du texte à propos des serpents')
-            ->setUser($this->getReference(UserFixtures::USER_USER2))
-            ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG2));
-        $manager->persist($pages2);
-
-        $pages2 = new Page();
-        $pages2->setTitle( 'Les ggg')
-            ->setContent('Du texte à propos des serpents')
-            ->setUser($this->getReference(UserFixtures::USER_USER2))
-            ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG2));
-        $manager->persist($pages2);
-
-        $pages2 = new Page();
-        $pages2->setTitle( 'Les zzzz')
-            ->setContent('Du texte à propos des serpents')
-            ->setUser($this->getReference(UserFixtures::USER_USER2))
-            ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG2));
-        $manager->persist($pages2);
-
-        $pages2 = new Page();
-        $pages2->setTitle( 'Les hhh')
-            ->setContent('Du texte à propos des serpents')
-            ->setUser($this->getReference(UserFixtures::USER_USER2))
-            ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG2));
-        $manager->persist($pages2);
-
         $pages3 = new Page();
-        $pages3->setTitle( 'les dinosaures')
-            ->setContent('Du texte à propos des lezards')
-            ->setUser($this->getReference(UserFixtures::USER_USER3))
-            ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG3));
+        $pages3->setTitle( 'Les ddd')
+            ->setContent('Du texte à propos des serpents')
+            ->setUser($this->getReference(UserFixtures::USER_USER2))
+            ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG2));
         $manager->persist($pages3);
 
         $pages4 = new Page();
-        $pages4->setTitle( 'crepe')
+        $pages4->setTitle( 'Les ggg')
+            ->setContent('Du texte à propos des serpents')
+            ->setUser($this->getReference(UserFixtures::USER_USER2))
+            ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG2));
+        $manager->persist($pages4);
+
+        $pages5 = new Page();
+        $pages5->setTitle( 'Les hhh')
+            ->setContent('Du texte à propos des serpents')
+            ->setUser($this->getReference(UserFixtures::USER_USER2))
+            ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG2));
+        $manager->persist($pages5);
+
+        $pages6 = new Page();
+        $pages6->setTitle( 'les dinosaures')
+            ->setContent('Du texte à propos des lezards')
+            ->setUser($this->getReference(UserFixtures::USER_USER3))
+            ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG3));
+        $manager->persist($pages6);
+
+        $pages7 = new Page();
+        $pages7->setTitle( 'crepe')
             ->setContent('Du texte à propos des crepess')
             ->setUser($this->getReference(UserFixtures::USER_USER3))
             ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG4));
-        $manager->persist($pages4);
+        $manager->persist($pages7);
+
+        $pages8 = new Page();
+        $pages8->setTitle( 'crepe')
+            ->setContent('Du texte à propos des crepess')
+            ->setUser($this->getReference(UserFixtures::USER_USER3))
+            ->setBlog($this->getReference(BlogFixtures::BLOG_BLOG4));
+        $manager->persist($pages8);
+
 
 
 
