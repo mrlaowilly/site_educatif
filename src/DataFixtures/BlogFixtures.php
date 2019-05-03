12e90f3d4d8a04e8e01ffb5fb9f6559d872fe9ee
Meksavanh Willy <mrlao93@hotmail.fr>
2019-05-03 14:17:02 +0200

    enelver fixture

--- src/DataFixtures/BlogFixtures.php
+++ src/DataFixtures/BlogFixtures.php
@@ -12,9 +12,6 @@ class BlogFixtures extends Fixture
     const BLOG_BLOG1 = 'blog1';
     const BLOG_BLOG2 = 'blog2';
     const BLOG_BLOG3 = 'blog3';
-    const BLOG_BLOG4 = 'blog4';
-    const BLOG_BLOG5 = 'blog5';
-    const BLOG_BLOG6 = 'blog6';
 
 
     public function load(ObjectManager $manager)
@@ -46,39 +43,12 @@ class BlogFixtures extends Fixture
 
         $manager->persist($blog3);
 
-        $blog1 = new Blog();
-        $blog1-> setTitle('Cuisine')
-            ->setDescriptif('Le premier article cuisine')
-            ->setAuthor('Pierre')
-            ->setImageLink('image/blog2.jpg');
-
-        $manager->persist($blog4);
-
-        $blog2 = new Blog();
-        $blog2-> setTitle('Sports nautique')
-            ->setDescriptif('Article Sports nautique')
-            ->setAuthor('Paul')
-            ->setImageLink('image/pic3.jpg');
-
-        $manager->persist($blog5);
-
-        $blog3 = new Blog();
-        $blog3-> setTitle('Jeux video')
-            ->setDescriptif('Le premier article jeux video')
-            ->setAuthor('Jacque')
-            ->setImageLink('image/blog3.jpg');
-
-        $manager->persist($blog6);
-
 
         $manager->flush();
 
         $this->addReference(self::BLOG_BLOG1, $blog1);
         $this->addReference(self::BLOG_BLOG2, $blog2);
         $this->addReference(self::BLOG_BLOG3, $blog3);
-        $this->addReference(self::BLOG_BLOG4, $blog4);
-        $this->addReference(self::BLOG_BLOG5, $blog5);
-        $this->addReference(self::BLOG_BLOG6, $blog6);
 
     }
 }
